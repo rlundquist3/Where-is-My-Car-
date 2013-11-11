d0b23c1deae8a8888b92bd4350217ffaf616069b
Riley Lundquist <rlundquist3@gmail.com>
2013-11-11 12:05:28 -0500

    GPS and Maps API added

--- src/com/example/whereismycar/MainActivity.java
+++ src/com/example/whereismycar/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.whereismycar;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+	
+	GPSTracker gps;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	
+	public void showLocation(View view) {
+		gps = new GPSTracker(this);
+		
+		if (gps.canGetLocation) {
+			double latitude = gps.getLatitude();
+			double longitude = gps.getLongitude();
+			
+			Toast.makeText(getApplicationContext(), "Current Location:\n" + latitude + "\n" + longitude, Toast.LENGTH_LONG).show();
+		}
+		else
+			gps.showSettingsAlert();
 	}
-
 }
